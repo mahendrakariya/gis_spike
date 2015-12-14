a5faf4d19da0f46e56b2324d5c9bd9f196c1003a
Rajeev N B <rajeevnb@thoughtworks.com>
2015-12-14 18:00:37 +0530

    RJ | Refactor code

--- src/read_write.go
+++ src/read_write.go
@@ -9,8 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const READ_MAX = 900
-const WRITE_MAX = 1000
+const READ_MAX = 100
 
 func main() {
 	db, err := sql.Open("postgres", "user=postgres password=123456 dbname=cabspike sslmode=disable")
@@ -20,15 +19,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	finished := make(chan int)
+	finished := make(chan bool)
 
 	for i := 0; i < READ_MAX; i++ {
-		go readDatabase(db, i, finished)
+		go readDatabase(db, finished)
 	}
 
-	// for i := 0; i < WRITE_MAX; i++ {
-	// go writeDatabase(db)
-	// }
 	var total_read_time int64
 	for i := 0; i < READ_MAX; i++ {
 		start := time.Now()
@@ -37,11 +33,11 @@ func main() {
 		start = time.Now()
 	}
 	avg_time_ns := total_read_time / READ_MAX
-	fmt.Printf("Avg read time: %v ms\n", avg_time_ns / int64(time.Millisecond))
-	fmt.Printf("Total read time for %v connections: %v ms\n", READ_MAX, total_read_time/int64(time.Millisecond)) 
+	fmt.Printf("Avg read time: %v ms\n", avg_time_ns/int64(time.Millisecond))
+	fmt.Printf("Total read time for %v connections: %v ms\n", READ_MAX, total_read_time/int64(time.Millisecond))
 }
 
-func readDatabase(db *sql.DB, i int, finished chan int) {
+func readDatabase(db *sql.DB, finished chan bool) {
 	rows, err := db.Query("SELECT count(*) from drivers")
 	if err != nil {
 		log.Fatal(err)
@@ -51,16 +47,20 @@ func readDatabase(db *sql.DB, i int, finished chan int) {
 	var count int
 	for rows.Next() {
 		err = rows.Scan(&count)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	finished <- count
-	//fmt.Printf("Rows %v", count)
+
+	finished <- true
 }
 
 func writeDatabase(db *sql.DB) {
 	result, err := db.Exec("UPDATE drivers set geog='POINT(77.6130574652 12.9018253405)' where driver_id=1")
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	res, err := result.RowsAffected()
-	fmt.Printf("RESULT %v", res)
+	fmt.Printf("Rows affected %v", res)
 }
