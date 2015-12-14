11a63ec9e6cd40bf96cba2a1fcc4a5f334924510
mahendrakariya <mahendrakariya@gmail.com>
2015-12-14 11:07:50 +0000

    calculating avg read query time

--- src/read_write.go
+++ src/read_write.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -12,7 +13,7 @@ const READ_MAX = 900
 const WRITE_MAX = 1000
 
 func main() {
-	db, err := sql.Open("postgres", "user=postgres dbname=geospat sslmode=disable")
+	db, err := sql.Open("postgres", "user=postgres password=123456 dbname=cabspike sslmode=disable")
 	defer db.Close()
 
 	if err != nil {
@@ -25,14 +26,19 @@ func main() {
 		go readDatabase(db, i, finished)
 	}
 
-	for i := 0; i < WRITE_MAX; i++ {
-		//go writeDatabase(db)
-	}
-
+	// for i := 0; i < WRITE_MAX; i++ {
+	// go writeDatabase(db)
+	// }
+	var total_read_time int64
 	for i := 0; i < READ_MAX; i++ {
-		res := <-finished
-		fmt.Printf("Result %v\n", res)
+		start := time.Now()
+		<-finished
+		total_read_time += time.Since(start).Nanoseconds()
+		start = time.Now()
 	}
+	avg_time_ns := total_read_time / READ_MAX
+	fmt.Printf("Avg read time: %v ms\n", avg_time_ns / int64(time.Millisecond))
+	fmt.Printf("Total read time for %v connections: %v ms\n", READ_MAX, total_read_time/int64(time.Millisecond)) 
 }
 
 func readDatabase(db *sql.DB, i int, finished chan int) {
